46a146d71bfc3bbb0dcf18bc7ea56ab41dbd68b2
Ayan_0011 <ayanansari9235@gmail.com>
2025-09-20 15:32:32 +0530

    first commit

---
+++ Project/Portfolio/script.js
@@ -0,0 +1,15 @@
+  //<!--hide/show navbar -->
+  var navlink = document.getElementById("navlink");
+        function showmenu() {
+            navlink.style.display = "block";
+        }
+        function hidemenu() {
+            navlink.style.display = "none";
+        }
+   var typed = new Typed('#element', {
+            strings: ['Frontend Devloper', 'Web Design', 'Web Devloper'],
+            typeSpeed: 50,
+            backSpeed: 30,
+            loop: true
+
+        });
