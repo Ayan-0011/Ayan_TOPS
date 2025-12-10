367d23f78495477f5e2481946716c7d1d6658192
Ayan_0011 <ayanansari9235@gmail.com>
2025-12-10 20:00:12 +0530

    first commit

--- Project/React-project/src/Components/FilterSection.jsx
+++ Project/React-project/src/Components/FilterSection.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { getData } from '../Context/DataContext';
 
-const FilterSection = () => {
+const FilterSection = ({serch, setSerch, Category, setCategory, priceRange, setPrinceRange, handlecategoryChange}) => {
 
   const { categoryData, fetchCategories } = getData()
 
@@ -12,35 +12,36 @@ const FilterSection = () => {
   //console.log(categoryData);
 
 
-
   return (
     <div className='bg-gray-300 mt-5 p-4 rounded-md h-[530px] filterbae'>
-      <input type="text" placeholder='Serch...' className='p-2 bg-white border-2 border-gray-400 rounded-md' />
+      <input type="text" placeholder='Serch...' value={serch}  onChange={ (e) => setSerch(e.target.value)}
+      className='p-2 bg-white border-2 border-gray-400 rounded-md' />
 
       {/* category only data */}
       <h1 className='mt-5 font-semibold text-xl'>Category</h1>
       <div className="flex flex-col gap-2  mt-3">
 
-        <div className='flex gap-2'>
-          <input type="checkbox" name="" id="" value="All" />
-          <button>All</button>
-        </div>
-
+      <div className='flex'>
+          <input type="checkbox" checked/>
+          <button>ALL</button>
+      </div>
         {
-          categoryData.map((item, index) => {
+          categoryData?.map((item, index) => {
             return <div key={index} className='flex gap-2'>
-              <input type="checkbox" />
-              <button className='uppercase'>{item.name}</button>
+              <input type="checkbox" name={item} checked={ Category === item.name} value={item.name} onChange={handlecategoryChange} />
+              <button onClick={() => setCategory(item.name)} className='uppercase'>{item.name}</button>
             </div>
           })
         }
       </div>
       <h1 className='mt-5 font-semibold text-xl'>Range</h1>
-      <div className='felx flex-col gap-2 mt-3'>
-        <label>Price Range: $0-$500</label>
-        <input type="range" name="" id="" />
+      <div className='flex flex-col gap-2 mt-3'>
+        <label>Price Range: ${priceRange[0]} - ${priceRange[1]}</label>
+        <input type="range" name="" id="" value={priceRange[1]} onChange={(e)=>setPrinceRange([priceRange[0], Number(e.target.value)])} className='transition-all' />
       </div>
-      <button className='bg-red-500 p-2 text-white rounded-md mx-10 my-5'>Reset Filter</button>
+      <button
+      onClick={()=>{setSerch(''); setCategory('ALL'); setPrinceRange([0,100])}}
+      className='bg-red-500 p-2 text-white rounded-md mx-10 my-5'>Reset Filter</button>
     </div>
   )
 }
--- Project/React-project/src/pages/Products.jsx
+++ Project/React-project/src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getData } from '../Context/DataContext'
 import FilterSection from '../Components/FilterSection';
 import Loading from "../assets/Loading4.webm";
@@ -8,13 +8,28 @@ import ProductsCard from '../Components/ProductsCard';
 const Products = () => {
 
   const { data, FetchAllproducts } = getData()
+  const [serch, setSerch] = useState("");
+  const [Category, setCategory] = useState("All");
+  const [priceRange, setPriceRange] = useState([0,100]);
 
 
   useEffect(() => {
     FetchAllproducts()
   }, []);
   //console.log(data);
-  
+
+  const handlecategoryChange = (e)=>{
+      setCategory(e.target.value)
+      console.log(Category);
+      
+  }
+
+  const filteredData = data?.filter((item) =>
+    item.title.toLowerCase().includes(serch.toLowerCase()) &&
+    (Category === Category || item.Category?.name === Category) &&
+    item.price >= priceRange[0] && item.price <= priceRange[1]
+
+  )
 
   return (
     <div>
@@ -22,10 +37,11 @@ const Products = () => {
         {
           data?.length > 0 ? (
             <div className='flex gap-8'>
-              <FilterSection />
+              <FilterSection serch={serch} setSerch={setSerch} Category={Category} setCategory={setCategory} setPrinceRange={setPriceRange}  priceRange={priceRange} 
+              handlecategoryChange={handlecategoryChange} />
               <div className='grid grid-cols-4 gap-8 mt-5 '>
                 {
-                  data?.map((products, index) => {
+                  filteredData?.map((products, index) => {
                     return <ProductsCard key={index} products={products} />
                   })
                 }
@@ -36,7 +52,6 @@ const Products = () => {
               <video muted autoPlay loop>
                 <source src={Loading} type='video/webm' />
               </video>
-
             </div>
           )
         }
