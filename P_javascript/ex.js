e877af6c3f451bf758de7cd838903c2c345048e2
Ayan_0011 <ayanansari9235@gmail.com>
2026-02-05 11:02:01 +0530

    advanced react assigment

---
+++ P_javascript/ex.js
@@ -0,0 +1,16 @@
+﻿// const array = [1, 2, 3, 4, 5];
+
+// const doubled = array.map(num => num * 2);
+// const filter = array.filter(num => num % 2 === 0);
+// const reduce = array.reduce((acc, num) => acc + num, 0);
+
+
+// console.log(reduce); // Output: 15   
+
+const rev = [10,20,30,40,50];
+const res=[];
+
+for(let i=rev.length-1;i>=0;i--){
+    res.push(rev[i]);
+}
+comsole.log(res); // Output: [50,40,30,20,10]
