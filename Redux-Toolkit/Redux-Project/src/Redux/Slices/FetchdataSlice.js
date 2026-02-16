f8a62489159c2d61daae1fec0eba2ca9193cf14d
Ayan_0011 <ayanansari9235@gmail.com>
2026-02-16 18:01:53 +0530

    nivia sport clone added

--- Redux-Toolkit/Redux-Project/src/Redux/Slices/FetchdataSlice.js
+++ Redux-Toolkit/Redux-Project/src/Redux/Slices/FetchdataSlice.js
@@ -1,5 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { asyncThunkCreator, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios'
 
+
+const fetchdata = asyncThunkCreator = ()=>{
+    const res = axios.get("http://localhost:5001/Users");
+    console.log(res.data);
+    
+}
 export const data = createSlice({
     name:"Fetch data",
     initialState:{
